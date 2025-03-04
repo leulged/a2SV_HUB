753e49176b8371eb6d41458c631bd793932bb3dc
Leul Gedion <138241836+leulged@users.noreply.github.com>
2025-03-04 21:29:54 +0300

    Time: 182 ms (19.71%) | Memory: 51.4 MB (34.75%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -3,8 +3,10 @@ class Solution:
         points.sort()
         prev = points[0][1]
         count = 0
+        
         if len(points) == 1:
             return 1
+
         for i in range(1 , len(points)):
             if prev < points[i][0]:
                 count += 1
