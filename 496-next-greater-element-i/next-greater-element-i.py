8414d8243a793e7d6ad3b92adae0ff922f5472f5
Leul Gedion <138241836+leulged@users.noreply.github.com>
2025-03-06 18:04:26 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (98.17%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -3,7 +3,9 @@ class Solution:
         res = []
         my_dic = {}
         for i in range( len(nums2)):
+
             if res:
+                
                 while res and nums2[i] > res[-1]:
                     x = res.pop()
                     my_dic[x] = nums2[i]
@@ -14,8 +16,7 @@ class Solution:
             else:
                     res.append(nums2[i])
         ans = []
-        print(res)
-        print(my_dic)
+    
         for num in nums1:
             if num in res:
                 ans.append(-1)
