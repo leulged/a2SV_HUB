300d3dbe94acc960bc100787cbbad82673fa5d32
Leul Gedion <138241836+leulged@users.noreply.github.com>
2025-03-06 18:02:33 +0300

    Time: 2 ms (47.09%) | Memory: 18.2 MB (21.66%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,26 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = []
+        my_dic = {}
+        for i in range( len(nums2)):
+            if res:
+                while res and nums2[i] > res[-1]:
+                    x = res.pop()
+                    my_dic[x] = nums2[i]
+                    
+                    
+                res.append( nums2[i])
+                
+            else:
+                    res.append(nums2[i])
+        ans = []
+        print(res)
+        print(my_dic)
+        for num in nums1:
+            if num in res:
+                ans.append(-1)
+            
+            else:
+                ans.append(my_dic[num])
+              
+        return ans
