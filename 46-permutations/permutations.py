099c967f3a5dfb763bc819e6b3bf51922f88545c
Leul Gedion <138241836+leulged@users.noreply.github.com>
2025-03-21 12:28:00 +0300

    Time: 0 ms (100.00%) | Memory: 18.2 MB (14.96%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,7 +1,17 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        for perm in permutations(nums, len(nums)):
-            ans.append(perm)
-        
+        form = []
+        def perm(start):
+            if start == len(nums):
+                ans.append(nums[:])
+                return 
+            
+            for i in range(start , len(nums)):
+                nums[start] , nums[i] = nums[i] , nums[start]
+                perm(start + 1)
+                nums[start] , nums[i] = nums[i] , nums[start]
+
+        perm(0)
+
         return ans
