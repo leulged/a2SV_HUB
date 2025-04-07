a63bb827b561096d33772ad936b9ec491ad7b6af
Leul Gedion <138241836+leulged@users.noreply.github.com>
2025-04-07 19:56:34 +0300

    Time: 597 ms (47.06%) | Memory: 132.1 MB (41.96%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -11,11 +11,14 @@ class Solution:
         def dfs(node):
             if node == destination:
                 return True
+
             visited[node] = True
             for neighbor in graph[node]:
                 if not visited[neighbor]:
+
                     if dfs(neighbor):
                         return True
+                        
             return False
 
         return dfs(source)
