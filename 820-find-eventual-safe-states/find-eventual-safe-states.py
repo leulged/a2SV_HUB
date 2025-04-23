a73cce7d6fe0ec2cb90b9693d74c6d3ec0377c54
Leul Gedion <138241836+leulged@users.noreply.github.com>
2025-04-23 19:45:54 +0300

    Time: 72 ms (22.20%) | Memory: 23.9 MB (40.10%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,25 +1,26 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         n = len(graph)
-        reverse_graph = defaultdict(list)
-        out_degree = [0] * n
-        safe = [False for _ in range(n)]
-        for u in range(n):
-            out_degree[u] = len(graph[u])
-            for v in graph[u]:
-                reverse_graph[v].append(u)
-        
         queue = deque()
-        for i in range(len(out_degree)):
+        out_degree = [len(graph[i]) for i in range(n)]
+        back = defaultdict(list)
+
+        for i in range(n):
+            for j in graph[i]:
+                back[j].append(i)
+
+        for i in range(n):
             if out_degree[i] == 0:
                 queue.append(i)
-        
+
+        safe = [False] * n
         while queue:
             node = queue.popleft()
             safe[node] = True
-            for nei in reverse_graph[node]:
+
+            for nei in back[node]:
                 out_degree[nei] -= 1
                 if out_degree[nei] == 0:
                     queue.append(nei)
-        
-        return [i for i in range(n) if safe[i] == True]
+
+        return [i for i in range(n) if safe[i]]
