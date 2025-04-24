9544be8d3b969ac38ed706d347f595a1714cc585
Leul Gedion <138241836+leulged@users.noreply.github.com>
2025-04-24 09:14:06 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (68.84%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,6 +1,6 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        dir = [(0 , 1) , (1 , 0) , (0 , -1) , (-1 , 0)]
+        dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         val = image[sr][sc]
         n = len(image)
         m = len(image[0])
@@ -8,21 +8,20 @@ class Solution:
         if val == color:
             return image
 
-        def inbound (row , col):
+        def inbound(row, col):
             return 0 <= row < n and 0 <= col < m
-            
-        visited = [[False for _ in range(m)] for _ in range(n)]
 
-        def dfs(row , col):
-            visited[row][col] = True
-            image[row][col] = color
+        queue = deque()
+        queue.append((sr, sc))
+        image[sr][sc] = color 
 
-            for x , y in dir:
-                new_row = x + row
-                new_col = y + col
-                if inbound(new_row , new_col) and not visited[new_row][new_col] and image[new_row][new_col] == val:
-                    dfs(new_row , new_col)
-            
-            
-        dfs(sr , sc)
+        while queue:
+            row, col = queue.popleft()
+            for dx, dy in dir:
+                new_row = row + dx
+                new_col = col + dy
+                if inbound(new_row, new_col) and image[new_row][new_col] == val:
+                    image[new_row][new_col] = color
+                    queue.append((new_row, new_col))
+
         return image
