1f653aa115cbbe41b9907db650eb6fdf5d210e34
Leul Gedion <138241836+leulged@users.noreply.github.com>
2025-04-25 20:12:05 +0300

    Time: 127 ms (40.67%) | Memory: 20.4 MB (64.10%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -6,16 +6,13 @@ class Solution:
         graph = defaultdict(list)
         indegree = [0] * numCourses
         
-        # Create the prerequisite matrix
         preq = [[False] * numCourses for _ in range(numCourses)]
 
-        # Build graph and track indegrees
         for u, v in prerequisites:
             graph[u].append(v)
             indegree[v] += 1
-            preq[u][v] = True  # Direct prerequisite
+            preq[u][v] = True  
 
-        # Topological sort
         queue = deque(i for i in range(numCourses) if indegree[i] == 0)
 
         while queue:
@@ -23,15 +20,12 @@ class Solution:
             for neighbor in graph[curr]:
                 
                 for i in range(numCourses):
-                    if preq[i][curr]:  # if i is a prerequisite of curr
+                    if preq[i][curr]:  
                         preq[i][neighbor] = True
-                  # Also keep the direct one
-                # preq[curr][neighbor] = True
                 indegree[neighbor] -= 1
                 if indegree[neighbor] == 0:
                     queue.append(neighbor)
 
-        # Answer queries using the matrix
         ans = []
         for u, v in queries:
             ans.append(preq[u][v])
