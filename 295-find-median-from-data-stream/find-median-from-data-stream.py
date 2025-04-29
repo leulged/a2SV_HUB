d6a4fc4474b6bb37f02936bcc2984b4c98ccaceb
Leul Gedion <138241836+leulged@users.noreply.github.com>
2025-04-29 19:35:51 +0300

    Time: 142 ms (57.20%) | Memory: 39.5 MB (64.36%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,16 +1,20 @@
 class MedianFinder:
 
     def __init__(self):
-        self.arr = []
+        self.max_heap = []
+        self.min_heap = []
     def addNum(self, num: int) -> None:
-        self.arr.append(num)
+        heappush(self.max_heap , -num)
+        heappush(self.min_heap , -heappop(self.max_heap) )
+
+        if len(self.min_heap) > len(self.max_heap):
+            heappush(self.max_heap, -heappop(self.min_heap))
         
     def findMedian(self) -> float:
-        self.arr.sort()
-        if len(self.arr) % 2 == 0:
-            med = len(self.arr) // 2 
-            return (self.arr[med - 1] + self.arr[med]) / 2
-        return self.arr[len(self.arr) // 2 ]
+        if len(self.max_heap) > len(self.min_heap):
+            return -self.max_heap[0]
+        
+        return (-self.max_heap[0] + self.min_heap[0]) / 2
 
 
 
