7c0f32253703a37ed33b60c5554b53cff8629732
Leul Gedion <138241836+leulged@users.noreply.github.com>
2025-09-29 12:50:52 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (82.36%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -2,21 +2,15 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         memory = {}
         def dp(i):
-            if i == len(nums) - 2 or i == len(nums) - 1:
-                return nums[i]
+            if i >= len(nums):
+                return 0
 
             if i in memory:
                 return memory[i]
-
-            if i + 3 == len(nums):
-                memory[i] = nums[i] + dp(i + 2)
-            else:
-                memory[i] = nums[i] + max(dp(i + 2) , dp(i + 3))
+            
+            memory[i] = nums[i] + max(dp(i + 2) , dp(i + 3))
 
             return memory[i]
-
-        if len(nums) == 1:
-            return nums[0]
 
         return max(dp(0) , dp(1))
             
