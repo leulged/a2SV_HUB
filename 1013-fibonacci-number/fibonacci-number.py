3f3eaf5b2e2faefbb0eae2c4ca83633e93e47517
Leul Gedion <138241836+leulged@users.noreply.github.com>
2025-09-29 11:09:14 +0300

    Time: 476 ms (5.03%) | Memory: 18 MB (23.63%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,9 +1,14 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
+        memory = defaultdict(int)
+        def fibonnaci (i):
+            if i == 0 or i == 1 :
+                return i
+            
+            if memory[i] != 0 :
+                return memory[i]
+            
+            return fibonnaci(i - 1) + fibonnaci(i - 2)
         
-        if n == 1:
-            return 1
-
-        return self.fib(n - 1) + self.fib(n - 2)
+        return fibonnaci(n)
+        
